e61d46a42bf8c63fc0127969613c8afb514c3f3a
Natalie Komforti <nkomforti@gmail.com>
2018-10-20 03:39:48 +0000

    Concepts covered: calling functions, variables, user input

---
+++ 01-hello-world/program.py
@@ -0,0 +1,9 @@
+print ('-----------------------------------------------')
+print ('                   HELLO APP')
+print ('-----------------------------------------------')
+print ()
+
+user_response = input('What is your name? ')
+greeting = 'Nice to meet you, ' + user_response
+
+print (greeting)
